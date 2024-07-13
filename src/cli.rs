be831a22d78aa5acbad1c3defa2dcbe1b9f1e14b
Yann SALMON <yannsalmon.pro@gmail.com>
2024-07-13 23:23:00 +0200

    add prog description to help

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,11 @@ use clap::{Parser, Subcommand};
 #[derive(Parser, Debug)]
 #[command(version)]
 #[command(propagate_version = true)]
+#[clap(verbatim_doc_comment)]
+/// A command-line habit tracker.
+///
+/// Create a habit, log your reps and see your progress via
+/// cool terminal-based visualizations (yes, there is ASCII art!).
 pub struct Cli {
     #[command(subcommand)]
     pub command: Command,
