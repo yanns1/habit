1da38f4e7b2a45b9170766468fbcd707753debf3
Yann SALMON <yannsalmon.pro@gmail.com>
2024-07-29 09:22:56 +0200

    remove doc comments for centered_rect because failing tests

--- src/show/ui.rs
+++ src/show/ui.rs
@@ -1,10 +1,5 @@
 use ratatui::layout::{Constraint, Direction, Layout, Rect};
 
-/// # Usage
-///
-/// ```rust
-/// let rect = centered_rect(f.size(), 50, 50);
-/// ```
 pub fn centered_rect(r: Rect, percent_width: u16, percent_height: u16) -> Rect {
     let popup_layout = Layout::default()
         .direction(Direction::Vertical)
